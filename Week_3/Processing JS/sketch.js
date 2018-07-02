87ce6d7ad34860802e7aa3ccd2ca21f4bb8546ad
viviantran1 <viviantran654@gmail.com>
2018-07-02 14:01:09 -0700

    Circles

--- Week_3/Processing JS/sketch.js
+++ Week_3/Processing JS/sketch.js
@@ -1,15 +1,16 @@
 // define a circle object
 var circle = {
-    diameter: Math.random() * 50,
+    diameter: Math.random() * 50, // start with random diameter
     xCoor: 0,
     yCoor: 0,
-    color: [Math.random() * 255, Math.random() * 255, Math.random() * 255],
+    color: [Math.random() * 255, Math.random() * 255, Math.random() * 255], // start with random color
     xSpeed: 20,
     ySpeed: 10
 }
 
 function setup() {
     createCanvas(640, 480);
+    // random background color
     background(Math.random() * 255, Math.random() * 255, Math.random() * 255);
     frameRate(30);
 }
@@ -17,12 +18,13 @@ function setup() {
 function draw() {
     fill(circle.color);
     ellipse(circle.xCoor, circle.yCoor, circle.diameter);
-    console.log("The x speed is " + circle.xSpeed);
     
-    // if we hit right edge of canvas
+    // if we hit edge of canvas
     if(circle.xCoor > 640){
+        // random color
         circle.color = [Math.random() * 255, Math.random() * 255, Math.random() * 255]
-        circle.xSpeed = -circle.xSpeed; // reverse direction
+        // reverse direction
+        circle.xSpeed = -circle.xSpeed;
     } else if(circle.xCoor < 0){
         circle.color = [Math.random() * 255, Math.random() * 255, Math.random() * 255]
         circle.xSpeed = -circle.xSpeed;
@@ -34,6 +36,7 @@ function draw() {
         circle.ySpeed = -circle.ySpeed;
     }
     
+    // move circle across screen
     circle.xCoor += circle.xSpeed;
     circle.yCoor += circle.ySpeed;
 }
